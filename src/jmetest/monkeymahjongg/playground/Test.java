86c34eed1f2f7bd31b29f49d7c633dfa5e97690b
ablochberger <ablochberger@a3e2ebb7-6236-0410-bd38-6394eed6bc64>
2007-08-04 12:59:11 +0000

    board with original xml level format

--- src/jmetest/monkeymahjongg/playground/Test.java
+++ src/jmetest/monkeymahjongg/playground/Test.java
@@ -37,54 +37,14 @@ public class Test {
 	}
 
 	public static void main(String[] args) {
-		int layer[][][] = new int[][][] {
-				{ { 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
-						{ 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
-						{ 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
-						{ 0, 0, 1, 1, 1, 1, 1, 1, 0, 0 },
-						{ 0, 0, 1, 1, 1, 1, 1, 1, 0, 0 },
-						{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
-						{ 0, 0, 1, 1, 1, 1, 1, 1, 0, 0 },
-						{ 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
-						{ 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
-						{ 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 } },
-				{ { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-						{ 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
-						{ 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
-						{ 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
-						{ 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
-						{ 0, 1, 1, 1, 1, 1, 1, 1, 0, 0 },
-						{ 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
-						{ 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
-						{ 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
-						{ 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 } },
-				{ { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-						{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-						{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-						{ 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
-						{ 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
-						{ 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
-						{ 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
-						{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-						{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-						{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } } };
-
-		int count = Board.countTiles(layer);
-		if (count % 4 == 0) {
-			Board b = new Board(layer[0][0].length, layer[0].length,
-					layer.length);
-			b.setGroupCount(count / 4);
-			b.addTiles(layer);
-
-			int missingTileCount = b.missingTileCount();
-			if (missingTileCount == 0) {
-				System.out.println("board is valid");
-				enumerating(b);
-			} else {
-				System.err.println(String.format(
-						"board is not valid, %d tiles missing",
-						missingTileCount));
-			}
+		Board b = new Board( new XMLLevel( "level/standard.xml") );
+		int missingTileCount = b.missingTileCount();
+		if (missingTileCount == 0) {
+			System.out.println("board is valid");
+			enumerating(b);
+		} else {
+			System.err.println(String.format(
+					"board is not valid, %d tiles missing", missingTileCount));
 		}
 	}
 
